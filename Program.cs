c3abd23440e7707d1623fbfc56a4eb5cd2800646
2joaomiguel6 <102696102+2joaomiguel6@users.noreply.github.com>
2023-06-08 12:45:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ams_heranca_treeclass{
+class program{
+        public static void Main(string[] args)
+        {
+            Fly aguia = new Fly("224312", "Águia");
+            Walks pinguim = new Walks("224313", "Pinguim");
+
+            aguia.Voar();
+            System.Console.WriteLine($"----------------------------------");
+            pinguim.Voar();
+        }
+    }
+}
